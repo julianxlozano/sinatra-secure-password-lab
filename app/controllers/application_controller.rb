d91cb64bf7994751c18bb8a21e935683b17cae29
julianxlozano <julianxlozano@gmail.com>
2021-04-05 18:31:17 -0500

    commit tree 0c79fc5f5a2e51a7bb802cfed6f1f3243ef70c6e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,17 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+   if params["username"] == ""
+    redirect "/failure"
+   else
+    user = User.new(:username => params[:username], :password => params[:password])
+    #binding.pry 
+		if user.password != nil 
+			redirect "/login"
+		  else
+			redirect "/failure"
+		  end
+    end
   end
 
   get '/account' do
@@ -33,6 +42,13 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/account"
+		  else
+			redirect "/failure"
+		  end
   end
 
   get "/failure" do
